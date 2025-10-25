158cfb2e724164e273573d8eb20049b8723c368f
fullhumanbody <antstarlife@gmail.com>
2025-10-25 14:21:42 -0700

    lecture

---
+++ excercises/lab05.js
@@ -0,0 +1,31 @@
+function askNumber(whatNumber) {
+    let userNumber = prompt("Guess 1-10?");
+    if (userNumber == whatNumber) {
+    $("#output").html("You got it!");}
+    else {
+    $("#output").html("Noooooope") }
+
+
+}
+
+$("#good-button").click(function () {
+    askNumber(7);
+});
+
+
+
+function functionname(p1,p2,p3) {
+
+$("#lol").click(function(){
+    let all3p= p1+p2+p3
+
+    if (all3p >2) {
+
+        
+    }
+    
+
+})
+
+
+}
