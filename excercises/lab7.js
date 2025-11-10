1dacfe59f9834ad3e646dbe96b9b2409d359e082
fullhumanbody <antstarlife@gmail.com>
2025-11-10 13:38:31 -0800

    lab 7

---
+++ excercises/lab7.js
@@ -0,0 +1,12 @@
+$(".nice-block span").click(function () {
+
+    $(this).parent().toggleClass("more");
+
+    if ($(this).html() == "more") {
+        $(this).html("less");
+    } else {
+        $(this).html("more");
+    }
+
+});
+
